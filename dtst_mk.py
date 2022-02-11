663312024f1250e8c7add044fe1b631bbf53437a
alexunderch <73017461+alexunderch@users.noreply.github.com>
2022-02-11 10:53:03 +0300

    Update dtst_mk.py

--- dtst_mk.py
+++ dtst_mk.py
@@ -1,4 +1,4 @@
-from lib.linguistic_utils import BertProbingDataset
+from lib.linguistic_utils import BertProbingDataset, make_dataset
 import os
 def main():
     os.system("wget https://raw.githubusercontent.com/facebookresearch/SentEval/main/data/probing/past_present.txt")
@@ -9,7 +9,9 @@ def main():
     dtst = BertProbingDataset('bert', './past_present.txt', "tense_set", 'tense')
     _ = dtst.make_dataset()
 
+    linguistic_dataset = make_dataset()
+    #saving dataset dict on the disk
     del dtst
-
+    del linguistic_dataset
 
 if __name__ == "__main__": main()
